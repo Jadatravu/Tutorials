4c6422bd2e40a5b6a1f014a72dda2a2c64f28d06
jadatravu <surya.janardhan@gmail.com>
2016-10-29 20:48:31 -0400

    Django model/view updated

--- django_tutorials/Django1.10Pro/mypro1/myapp1/urls.py
+++ django_tutorials/Django1.10Pro/mypro1/myapp1/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.hello, name='hello'),
+    url(r'^h1$', views.hello, name='hello'),
+    url(r'^i1$', views.idata, name='idata'),
 ]
--- django_tutorials/Django1.10Pro/mypro1/myapp1/views.py
+++ django_tutorials/Django1.10Pro/mypro1/myapp1/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from myapp1.models import mymodel
 
 # Create your views here.
 
 def hello(request):
     return HttpResponse("<h2> This is my First Django Program </h2>")
+
+def idata(request):
+    var1_value = request.GET['var1']
+    var2_value = request.GET['var2']
+    m1 = mymodel(pid=var1_value, pdescription=var2_value)
+    m1.save()
+    return HttpResponse("<h2> Data Inserted </h2>")
